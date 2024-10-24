b24fb6b891655dc77f06e7dfe7f0b475458c69b2
Justin Martin Muñoz Escorcia <210659@utxicotepec.edu.mx>
2024-10-24 11:53:34 -0600

    creacion flutter StatefulWidget

--- helloworld_app_210659/lib/main.dart
+++ helloworld_app_210659/lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld_app_210659/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hola mundo desde flutter'),
-    );
-    throw UnimplementedError();
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorSchemeSeed: Colors.red
+        ),
+        home: const CounterScreen()
+        );
   }
 }
